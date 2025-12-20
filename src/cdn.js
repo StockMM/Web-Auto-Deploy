d55783d1d2933ac2ac6ed1a9a7bf96b31ec94f2d
zhangshijia <732147330@qq.com>
2025-12-20 23:19:31 +0800

    feat(cdn): add v2 request for cdn cache clearing
    Add a second request variant (V2) with updated URL format without the '3' prefix for improved compatibility

--- src/cdn.js
+++ src/cdn.js
@@ -9,6 +9,9 @@ module.exports = function (config) {
         const postData = querystring.stringify({
           url: '3http://stockmmm.com/, 3https://stockmmm.com/, 3http://www.stockmmm.com/, 3https://www.stockmmm.com/, 3http://www.stockmmm.com/,'
         })
+        const postDataV2 = querystring.stringify({
+          url: 'http://stockmmm.com/, https://stockmmm.com/, http://www.stockmmm.com/, https://www.stockmmm.com/, http://www.stockmmm.com/'
+        })
 
         const options = {
           protocol: 'https:',
@@ -30,7 +33,6 @@ module.exports = function (config) {
             resolve('cdn缓存清理成功')
           })
         })
-
         req.on('error', error => {
           console.error(error)
           reject('cdn缓存清理失败')
@@ -38,6 +40,22 @@ module.exports = function (config) {
 
         req.write(postData)
         req.end()
+
+        const reqV2 = https.request(options, res => {
+          res.on('data', d => {
+            // process.stdout.write(d)
+          })
+          res.on('end', () => {
+            // resolve('cdn缓存清理成功')
+          })
+        })
+        reqV2.on('error', error => {
+          console.error(error)
+          // reject('cdn缓存清理失败')
+        })
+
+        reqV2.write(postDataV2)
+        reqV2.end()
       })
     }
   }

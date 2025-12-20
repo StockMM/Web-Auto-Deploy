47ac4b71ad0f87a72180327716fb16024f000bc8
zhangshijia <732147330@qq.com>
2025-12-20 21:38:04 +0800

    feat(cdn): add cdn cache cleaning functionality
    Integrate CDN cache cleaning feature that triggers after web deployment. The new cdn.js module handles cache invalidation requests to the CDN provider. Also refactor string concatenation in index.js to improve message building flow.

---
+++ src/cdn.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const https = require('https')
+const querystring = require('querystring')
+
+module.exports = function (config) {
+  return {
+    cleanCache: new Promise((resolve, reject) => {
+      // Construct the form data correctly using querystring
+      // Fixed the '3' prefix typos and removed duplicates
+      const postData = querystring.stringify({
+        url: '3http://stockmmm.com/, 3https://stockmmm.com/, 3http://www.stockmmm.com/, 3https://www.stockmmm.com/, 3http://www.stockmmm.com/,'
+      })
+
+      const options = {
+        protocol: 'https:',
+        host: 'cp.huyuncdn.com',
+        path: '/vhost/?c=cacheclean&a=cacheclean',
+        method: 'POST',
+        headers: {
+          // Use correct content type for form data
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(postData)
+        }
+      }
+
+      const req = https.request(options, res => {
+        res.on('data', d => {
+          process.stdout.write(d)
+        })
+        res.on('end', () => {
+          resolve('cdn缓存清理成功')
+        })
+      })
+
+      req.on('error', error => {
+        console.error(error)
+        reject('cdn缓存清理失败')
+      })
+
+      req.write(postData)
+      req.end()
+    })
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-const core = require("@actions/core");
-const Message = require("./helpers/message");
-const web = require("./web");
+const core = require('@actions/core')
+const Message = require('./helpers/message')
+const web = require('./web')
+const cdn = require('./cdn')
 
 try {
   // 应用类型暂时只支持message、web
-  const type = core.getInput("type");
-  const robotKey = core.getInput("robotkey");
-  const content = core.getInput("content");
-  const input = core.getInput("input");
+  const type = core.getInput('type')
+  const robotKey = core.getInput('robotkey')
+  const content = core.getInput('content')
+  const input = core.getInput('input')
 
-  const message = new Message(robotKey);
+  const message = new Message(robotKey)
 
-  const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+  const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE
 
   switch (type) {
     // web应用
-    case "web":
-      var host = core.getInput("host");
-      var port = core.getInput("port");
-      var username = core.getInput("username");
-      var password = core.getInput("password");
-      var output = core.getInput("output");
-      var script = core.getInput("script");
-      var clean = core.getInput("clean") === "true";
-      var cleanPaths = core.getInput("cleanPaths");
+    case 'web':
+      var host = core.getInput('host')
+      var port = core.getInput('port')
+      var username = core.getInput('username')
+      var password = core.getInput('password')
+      var output = core.getInput('output')
+      var script = core.getInput('script')
+      var clean = core.getInput('clean') === 'true'
+      var cleanPaths = core.getInput('cleanPaths')
 
       // 处理清理路径
       const cleanPathsArray = cleanPaths
-        ? cleanPaths.split(",").map(p => p.trim()).filter(Boolean)
-        : [];
+        ? cleanPaths
+            .split(',')
+            .map(p => p.trim())
+            .filter(Boolean)
+        : []
 
+      var newContent = content
       web({
         host: host,
         port: port,
@@ -42,23 +47,34 @@ try {
         clean: clean,
         cleanPaths: cleanPathsArray
       })
-        .then((text) => {
-          message.sendText(content + '-' + text);
+        .then(text => {
+          newContent = newContent + '-' + text
+          cdn()
+            .cleanCache()
+            .then(txt => {
+              newContent = newContent + '(' + txt + ')'
+              message.sendText(newContent)
+            })
+            .catch(txt => {
+              newContent = newContent + '(' + txt + ')'
+              message.sendText(newContent)
+            })
         })
-        .catch((text) => {
-          message.sendText(content + '-' + text);
-        });
-      break;
+        .catch(text => {
+          newContent = newContent + '-' + text
+          message.sendText(newContent)
+        })
+      break
     // 发送消息
-    case "message":
+    case 'message':
       if (!content) {
-        return console.log("参数配置缺失，需要content");
+        return console.log('参数配置缺失，需要content')
       }
-      message.sendText(content);
-      break;
+      message.sendText(content)
+      break
     default:
-      console.log("没有该类型");
+      console.log('没有该类型')
   }
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
